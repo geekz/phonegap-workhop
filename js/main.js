a5a8eacfc0231a9fb63cb334bd9bde4f08dd9f19
Murali Kumar <geekmails@gmail.com>
2013-03-24 16:02:55 +0530

    forth commit

--- js/main.js
+++ js/main.js
@@ -14,10 +14,7 @@ var app = {
     },
 	
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+   
 	showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
